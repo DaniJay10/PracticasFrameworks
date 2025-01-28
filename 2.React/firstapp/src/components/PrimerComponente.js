809c7a691cdcb558bf081593d22643303b0a25ac
DaniJay10 <danielpinzon20jay@gmail.com>
2025-01-27 22:29:29 -0500

    PracticaFrameworks

---
+++ 2.React/firstapp/src/components/PrimerComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const PrimerComponente = () => {
+    let nombre = "Daniel Jay";
+    let edad = 21;
+  return (
+    <div>
+    <p>Mi nombre es {nombre}</p>
+    <p>Tengo {edad} años</p>
+    </div>
+  )
+}
